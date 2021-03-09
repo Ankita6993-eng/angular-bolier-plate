a44d9e019eeccc8993685cb5c0a919abd2870939
Chinmay Tagline <chinmay.tagline@gmail.com>
2021-03-09 18:44:15 +0530

    Registeration Validation and API Integration.

--- src/app/views/register/register.component.ts
+++ src/app/views/register/register.component.ts
@@ -1,37 +1,48 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
-import { AuthService } from '../../services/auth.service';
-import { StorageService } from '../../services/storage.service';
+import { Component } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
+import { AuthService } from "../../services/auth.service";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: 'register.component.html'
+  selector: "app-dashboard",
+  templateUrl: "register.component.html",
 })
 export class RegisterComponent {
   registerForm: FormGroup;
-  emailPattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$";
+  emailPattern =
+    "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$";
+  submitted = false;
+
   constructor(
-    public formBuilder: FormBuilder,
+    private formBuilder: FormBuilder,
     private auth: AuthService,
     private toaster: ToastrService,
     private router: Router
   ) {
-    this.registerForm = this.formBuilder.group({
-      firstname: ['', [Validators.required]],
-      lastname: ['', [Validators.required]],
-      email: ['', [Validators.required, Validators.email, Validators.pattern(this.emailPattern)]],
-      password: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
-      confirmPassword: ['', Validators.required]
-    },
-    {
-      validator: this.mustMatch('password', 'confirmPassword')
-    }
+    this.registerForm = this.formBuilder.group(
+      {
+        first_name: ["", [Validators.required]],
+        last_name: ["", [Validators.required]],
+        email: [
+          "",
+          [
+            Validators.required,
+            Validators.email,
+            Validators.pattern(this.emailPattern),
+          ],
+        ],
+        role: ["", [Validators.required]],
+        dob: ["", [Validators.required]],
+        password: ["", [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ["", [Validators.required, Validators.minLength(6)]],
+        gender: [""],
+      },
+      {
+        validator: this.mustMatch("password", "confirmPassword"),
+      }
     );
   }
-
-
   mustMatch(controlName: string, matchingControlName: string) {
     return (formGroup: FormGroup) => {
       const control = formGroup.controls[controlName];
@@ -50,21 +61,30 @@ export class RegisterComponent {
   }
 
   submit() {
-    if (this.registerForm.valid) {
-      const formData = this.registerForm.value;
-      this.auth.register(formData).subscribe((res: any) => {
-        if (res.statusCode === 200) {
-          StorageService.setData('token', res.data.token);
-          setTimeout(() => {
-            this.toaster.success('Welcome Admin!');
-            this.router.navigate(['/countries'])
-          }, 500);
-        } else {
-          this.toaster.error(res.message);
-        }
-      })
-      this.registerForm.reset();
+    this.submitted = true;
+    if (this.registerForm.invalid) {
+      return false;
     }
-  }
+    const tempData = this.registerForm.value;
+    delete tempData.confirmPassword;
 
+    tempData.dob = tempData.dob.split("-").join("/");
+
+    this.auth.register(tempData).subscribe(
+      (response: any) => {
+        if (response.statusCode === 200) {
+          this.toaster.success("Welcome Employee");
+          this.router.navigate(["/login"]);
+        } else {
+          this.toaster.error(response.message);
+        }
+      },
+      (error) => {
+        console.log(error);
+        this.toaster.error("Oops something went wrong.");
+      }
+    );
+    this.submitted = false;
+    this.registerForm.reset();
+  }
 }
