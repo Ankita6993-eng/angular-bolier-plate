fc663862afac2583be2703ca40e05e92224f1c7d
Chinmay Tagline <chinmay.tagline@gmail.com>
2021-03-23 18:33:22 +0530

    Project manager changes 23/3/2021

--- src/app/views/project-manager/project-manager-list/project-manager-list/project-manager-list.component.ts
+++ src/app/views/project-manager/project-manager-list/project-manager-list/project-manager-list.component.ts
@@ -2,10 +2,10 @@ import { Component, OnInit, ViewChild } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { ModalDirective } from "ngx-bootstrap/modal";
 import { BehaviorSubject, ReplaySubject } from "rxjs";
-import { takeUntil } from "rxjs/operators";
-import { EmployeesService } from "../../../../services/employees.service";
+import { debounce, take, takeUntil } from "rxjs/operators";
+import { EmployeesService, ResponseData } from "../../../../services/employees.service";
 import { ToastrService } from "ngx-toastr";
-import { AsyncPipe } from "@angular/common";
+import { AsyncPipe, formatDate } from "@angular/common";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { NgxUiLoaderService, SPINNER } from "ngx-ui-loader";
 @Component({
@@ -23,17 +23,19 @@ export class ProjectManagerListComponent implements OnInit {
   role: string;
   imageUrl:string;
   submitted=false;
-  selectedProjectManagerId;
+  selectedProjectManagerData;
   spinnerType = SPINNER.cubeGrid;
   image:File;
   imagesUrl:any="";
+  isEdit;
   emailPattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$";
   constructor(
     private employeeService: EmployeesService,
     private route: ActivatedRoute,
     private toaster: ToastrService,
     private fb:FormBuilder,
-    private ngxLoader:NgxUiLoaderService
+    private ngxLoader:NgxUiLoaderService,
+    private asyncPipe:AsyncPipe
   ) {
     this.role = this.route.snapshot.data.title;
     this.addProjectManagerForm = this.fb.group({
@@ -76,9 +78,109 @@ export class ProjectManagerListComponent implements OnInit {
       this.ngxLoader.stop();
     }
   }
+  formData(){
+    const formdata = new FormData;
+    const projectManagerData = this.addProjectManagerForm.value;
+    projectManagerData.role = this.role;
+    projectManagerData.dob = projectManagerData.dob.split("-").join("/");
+    Object.keys(projectManagerData).forEach((key) =>{
+      if(key != "image"){formdata.append(key,projectManagerData[key])}
+    });
+    if(this.image){
+      formdata.append("image",this.image);
+    }
+  }
+  addProjectManager(){
+    this.ngxLoader.start();    
+    const formdata = new FormData;
+    const projectManagerData = this.addProjectManagerForm.value;
+    projectManagerData.role = this.role;
+    projectManagerData.dob = projectManagerData.dob.split("-").join("/");
+    Object.keys(projectManagerData).forEach((key) =>{
+      if(key != "image"){formdata.append(key,projectManagerData[key])}
+    });
+    if(this.image){
+      formdata.append("image",this.image);
+    }
+    //this.image = null;
+    try{
+      this.employeeService.addEmployee(formdata).pipe(takeUntil(this.destroyed$)).subscribe((res:ResponseData)=>{
+        const {statusCode,data,message} = res;
+          if(statusCode == 200){
+            let projManager = this.projectManager$.value;
+            projManager.unshift(data);
+            projManager.pop();
+            //this.projectManager$.next(data);
+            this.toaster.success(message);
+            this.ngxLoader.stop();
+          }else{
+            this.toaster.error(message);
+            this.ngxLoader.stop();
+          }
+      });
+    }catch{
+      this.toaster.error();
+      this.ngxLoader.stop();
+    }
+  }
 
+  openUpdateProjectManagerForm(data:any){
+    this.projectManagerModal.show();
+    let date = formatDate(data.dob,"yyyy-MM-dd","en-US");
+    this.addProjectManagerForm.patchValue({
+      first_name:data.first_name,
+      last_name:data.last_name,
+      email:data.email,
+      dob:date,
+      password:data.password,
+      profile_pic:data.profile_pic,
+      gender:data.gender
+    })
+    this.imagesUrl = data.profile_pic;
+    this.isEdit = true;
+    this.selectedProjectManagerData = data._id;
+  }
+
+  updateProjectManager(){
+    //this.ngxLoader.start();
+    const formdata = new FormData;
+    const projectManagerData = this.addProjectManagerForm.value;
+    projectManagerData.role = this.role;
+    projectManagerData.dob = projectManagerData.dob.split("-").join("/");
+    Object.keys(projectManagerData).forEach((key) =>{
+      if(key != "image"){formdata.append(key,projectManagerData[key])}
+    });
+    if(this.image){
+      formdata.append("image",this.image);
+    }  
+    try{
+      this.employeeService.updateEmployee(formdata,this.selectedProjectManagerData).pipe(takeUntil(this.destroyed$)).subscribe((res:ResponseData)=>{
+        const {statusCode,data} = res;
+        if(statusCode == 200){         
+          let projManagerDataValue = this.asyncPipe.transform(this.projectManager$);
+          //console.log(projectManagerData);
+          let updatedProjManager = projManagerDataValue.find(item=>item._id == data._id);
+          console.log("updated proj value:",updatedProjManager);
+          let index = updatedProjManager.indexOf(projManagerDataValue);
+          console.log("index:",index);
+          if(index > -1){
+            projManagerDataValue[index]=data;
+            this.projectManager$.next(projManagerDataValue);
+            this.toaster.success("Updated Successfully");
+            this.closeProjectManagerModal();
+           // this.ngxLoader.stop();
+          }
+          else{
+            this.toaster.error("Cannot update record");
+          //  this.ngxLoader.stop();
+          }
+        }
+      })
+    }catch{
+
+    }
+  }
   uploadFile(event){
-
     let freader = new FileReader;
     if(event.target.files && event.target.files[0]){
     this.image = event.target.files[0];
@@ -95,20 +197,27 @@ export class ProjectManagerListComponent implements OnInit {
 
   onSubmit(){
     this.submitted = true;
-    if(!this.addProjectManagerForm.valid){
-      console.log("error");
+    if(this.addProjectManagerForm.invalid){
+      return false;
     }
-    console.log(this.addProjectManagerForm.value);
-    // this.addProjectManagerForm.reset();
-    // this.closeConfirmationModal();
+    this.isEdit ? this.updateProjectManager() : this.addProjectManager();
+    this.closeProjectManagerModal();
   }
   
-  openUpdateTraineeForm(){}
-  openConfirmationModal(){}
-  closeConfirmationModal(){}
+  
+  openConfirmationModal(){
+    this.confirmationModal.show();
+  }
+  closeConfirmationModal(){
+    this.confirmationModal.hide();
+  }
   closeProjectManagerModal(){
+    this.submitted = false;
+    this.imagesUrl = "";
     this.addProjectManagerForm.reset();
     this.projectManagerModal.hide();
+    this.selectedProjectManagerData = null;
+    
   }
   deleteProjectManager(){}
 }
