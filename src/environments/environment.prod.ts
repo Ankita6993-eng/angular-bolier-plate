6e054ad67eab81566126845310ecc2cb9f2cdff7
Ankita Anghan <ankitaa.tagline@gmail.com>
2021-05-06 16:26:29 +0530

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://anguleremployeedemo.herokuapp.com', // server using ngrok is temporary as of now
+  apiUrl: 'https://anguleremployeedemo.herokuapp.com',
   cryptoSecret: 'e1dc7fbc-45e5-4658-bd8e-5b4dcadb9b61',
 };
